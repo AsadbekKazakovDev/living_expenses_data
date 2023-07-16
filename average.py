6782d4fc8287ae09d4a48c0629a03e1ad0c02212
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-07-16 21:46:15 +0500

    solve main

--- average.py
+++ average.py
@@ -10,8 +10,15 @@ def average_expenses(file_path: str) -> float:
     Returns:
         float: average expenses
     """
-    pass
-
+    file_path=open('data.json')
+    load=json.load(file_path)
+    ans=0
+    bus=0
+    res=load.values()
+    for i in res:
+        bus+=i
+        ans+=1
+    return bus/ans
 # test
 file_path = "data.json"
 average = average_expenses(file_path)
